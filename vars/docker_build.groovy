0ad89322a5c398cf85d9296d0a3e3f5ad15b57d4
Devendra kumar vishwakarma <93032198+DeVeNdRa1910@users.noreply.github.com>
2025-03-01 03:41:47 +0530

    Update docker_build.groovy
    Correct name of Docker Image

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,5 +1,5 @@
-def docker_build(String ImageName, String ImageTag, String DockerHubUserName){
+def docker_build(String ImageName, String ImageTag){
   echo "Start building image"
-  sh "docker build -t ${DockerHubUserName}/${ImageName}:${ImageTag}"
+  sh "docker build -t ${ImageName}:${ImageTag}"
   echo "Docker image built successsfully"
 }
