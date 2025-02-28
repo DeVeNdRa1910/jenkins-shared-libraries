c9b9fa55961839c0cf5a6d1b1c5fe5eae0d7a6a5
Devendra kumar vishwakarma <93032198+DeVeNdRa1910@users.noreply.github.com>
2025-03-01 03:18:50 +0530

    Update clone.groovy
    add one more syntax

--- vars/clone.groovy
+++ vars/clone.groovy
@@ -1,5 +1,7 @@
 def clone(String giturl, String branch){
   echo "This is cloning the code"
   git url : giturl , branch : branch
+  // or
+  // git url : "${giturl}" , branch : "${branch}"
   echo "code cloning successfull"
 }
