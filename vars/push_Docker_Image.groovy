87a9fc7e63a29799963e8aa9c674a9e11e7efa5e
Devendra kumar vishwakarma <93032198+DeVeNdRa1910@users.noreply.github.com>
2025-03-04 00:14:40 +0530

    remove unwanted parameter

--- vars/push_Docker_Image.groovy
+++ vars/push_Docker_Image.groovy
@@ -1,4 +1,4 @@
-def call(Sringe ImageName, String ImageTag, String credentialsId, String passwordVariable, String usernameVariable){
+def call(Sringe ImageName, String ImageTag, String credentialsId){
   withCredentials([usernamePassword(credentialsId: dockerHubCred,passwordVariable: dockerHubPass,usernameVariable: dockerHubUser)]){
       echo "start pushing Image on Docker Hub"
       sh "docker login -u ${usernameVariable} -p ${passwordVariable}"
