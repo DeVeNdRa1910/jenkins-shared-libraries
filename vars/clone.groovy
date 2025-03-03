926ec43789d6ee9950af1f6d4bed2c0a3585076b
Devendra kumar vishwakarma <93032198+DeVeNdRa1910@users.noreply.github.com>
2025-03-03 23:59:44 +0530

    Update clone.groovy by doing some changes

--- vars/clone.groovy
+++ vars/clone.groovy
@@ -1,5 +1,6 @@
-def clone(String giturl, String branch){
+def clone(String giturl, String branch, , String credentialsId = null){
   echo "This is cloning the code"
+  echo "Cloning repository: ${giturl}, branch: ${branch}"
   git url : giturl , branch : branch
   // or
   // git url : "${giturl}" , branch : "${branch}"
