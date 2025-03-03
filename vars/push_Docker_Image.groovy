b5749df5390b7ae99155afcdc9280ccd3991cddb
Devendra kumar vishwakarma <93032198+DeVeNdRa1910@users.noreply.github.com>
2025-03-04 00:16:11 +0530

    try to resolve some error

--- vars/push_Docker_Image.groovy
+++ vars/push_Docker_Image.groovy
@@ -1,5 +1,5 @@
 def call(Sringe ImageName, String ImageTag, String credentialsId){
-  withCredentials([usernamePassword(credentialsId: dockerHubCred,passwordVariable: dockerHubPass,usernameVariable: dockerHubUser)]){
+  withCredentials([usernamePassword(credentialsId: 'dockerHubCred',passwordVariable: 'dockerHubPass',usernameVariable: 'dockerHubUser')]){
       echo "start pushing Image on Docker Hub"
       sh "docker login -u ${usernameVariable} -p ${passwordVariable}"
       sh "docker tag ${ImageName}:${ImageTag} ${usernameVariable}/${ImageName}:${ImageTag}"
