867995f034b2fa9cecdf05ab14a9b9da17a3640f
Devendra kumar vishwakarma <93032198+DeVeNdRa1910@users.noreply.github.com>
2025-03-04 00:33:50 +0530

    Change the syntax

--- vars/clone.groovy
+++ vars/clone.groovy
@@ -1,8 +1,8 @@
 def call(String giturl, String branch, , String credentialsId = null){
   echo "This is cloning the code"
   echo "Cloning repository: ${giturl}, branch: ${branch}"
-  git url : giturl , branch : branch
+  //git url : giturl , branch : branch
   // or
-  // git url : "${giturl}" , branch : "${branch}"
+  git url : "${giturl}" , branch : "${branch}"
   echo "code cloning successfull"
 }
