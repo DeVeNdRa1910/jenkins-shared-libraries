4168ec78b70cd084aa63125a0036513f48a816f0
Devendra kumar vishwakarma <93032198+DeVeNdRa1910@users.noreply.github.com>
2025-03-04 00:03:05 +0530

    Change the function name

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,4 +1,4 @@
-def docker_build(String ImageName, String ImageTag){
+def call(String ImageName, String ImageTag){
   echo "Start building image"
   sh "docker build -t ${ImageName}:${ImageTag}"
   echo "Docker image built successsfully"
